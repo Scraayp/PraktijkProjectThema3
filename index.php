609e36fdf0a17897d7d254918382cc1b94ae5769
Michal <scraayp@gmail.com>
2023-03-17 13:51:17 +0100

    add the folder structure

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * User: Michal Kolasa
+ * Date: 17/03/2023 13:50
+ * File: index.php
+ * 
+ * Omschrijving: De hoofdpagina van de website
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+            index.php
+        </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Koppeling CSS -->
+        <link rel="stylesheet" href="styles/stylesheet.css">
+        <!-- Koppeling JavaScript -->
+        <script src="scripts/script.js" defer></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
