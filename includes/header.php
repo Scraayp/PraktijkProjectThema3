40998b50ff9f042ffb41c3a9cecbb590915bf47f
dani-de-rooij <dani.derooij@edu-kw1c.nl>
2023-03-21 09:46:43 +0100

    - Header aangemaakt - Plaatjes toegevoegd - Stylesheet aangepast voor header - lettertype toegevoegd

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+    <header>
+        <nav>
+            <div id="navbar-left">
+                <img src="../images/psycholoognl.png">
+            </div>
+            <div id="navbar-right">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Psychologen en locaties</a></li>
+                    <li><a href="">Behandelingen</a></li>
+                    <li><a href="">Vergoedingen</a></li>
+                    <li><a href="">Meer weten</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">Persoonlijkheidstest</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
