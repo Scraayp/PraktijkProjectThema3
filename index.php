bdcaf97285a922ef76a6b6127c435f79176b8d17
dani-de-rooij <dani.derooij@edu-kw1c.nl>
2023-03-22 15:07:12 +0100

    Hoofdpagina deel 1 af

--- index.php
+++ index.php
@@ -18,13 +18,76 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Koppeling CSS -->
         <link rel="stylesheet" href="styles/stylesheet.css">
+        <link rel="stylesheet" href="styles/index.css">
         <!-- Koppeling JavaScript -->
         <script src="scripts/script.js" defer></script>
     </head>
     <body>
+<!--    Include de navigatie balk-->
     <?php
     include("includes/header.php");
     ?>
-        
+        <main>
+<!--            Banner met het plaatje ering die bovenaan de pagina staat           -->
+            <div id="mainBanner">
+                <div id="textContainer">
+                    <h1>
+                        Psycholoog Nederland
+                    </h1>
+                    <h2>
+                        Helpt je weer de juiste richting op.
+                    </h2>
+                    <p>
+                        <strong>
+                        Wij zijn een GGZ-instelling met monodisciplinaire en de multidisciplinaire behandelingen.
+                        Wij streven ernaar geen wachtlijsten te hebben.
+                        Wij behandelen volwassenen met o.a. angsten, depressie en somatoforme klachten.
+                        </strong>
+                    </p>
+                    <div id="buttons">
+                        <a href="" class="button">Neem contact op</a>
+                        <a href="pages/" class="button">doe de zelftest!</a>
+                    </div>
+                </div>
+            </div>
+<!--            Banner met de 3 blokjes text            -->
+            <div id="articles">
+                <div class="article">
+                    <h2>
+                        Geen wachtlijst
+                    </h2>
+                    <p>
+                        Wij zijn een GGZ-instelling met monodisciplinaire en de multidisciplinaire behandelingen.
+                        Wij streven ernaar geen wachtlijsten te hebben.
+                        Wij behandelen volwassenen met o.a. angsten, somatoforme klachten, depressie en persoonlijkheidsproblematiek
+                    </p>
+                </div>
+                <div class="article">
+                    <h2>
+                        Vergoeding
+                    </h2>
+                    <p>
+                        Onze behandelingen worden vanuit de basisverzekering vergoed.
+                        Je hebt hiervoor een verwijsbrief van de huisarts nodig.
+                        Afhankelijk van je zorgverzekering wordt alles of een deel vergoed.
+                        Kijk hiervoor bij ‘vergoedingen’ of een eigen bijdrage van toepassing is.
+                    </p>
+                </div>
+                <div class="article">
+                    <h2>
+                        Onze locaties
+                    </h2>
+                    <p>
+                        Psycholoog Nederland heeft meerdere locaties in het land. Onze grootste vestigingen zijn in Amsterdam, Utrecht, Hilversum en Rotterdam.
+                        Hier zitten behandelteams van rond de 15 tot 20 behandelaren. Je kunt meestal binnen één week al terecht voor een intake.
+                    </p>
+                </div>
+            </div>
+            <!--                Grijze vlak                     -->
+            <div id="grijs">
+            </div>
+<!--                            TextVlak                        -->
+        </main>
+
     </body>
 </html>
