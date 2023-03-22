566b54bfc0f049abee0cc5d13c335f48568a1804
dani-de-rooij <dani.derooij@edu-kw1c.nl>
2023-03-22 12:06:33 +0100

    5_2

--- includes/header.php
+++ includes/header.php
@@ -5,11 +5,11 @@
     <header>
         <nav>
             <div id="navbar-left">
-                <img src="/Praktijk/PraktijkProjectThema3psycholoognl.png">
+                <img src="/Praktijk/Project/PraktijkProjectThema3/images/psycholoognl.png">
             </div>
             <div id="navbar-right">
                 <ul>
-                    <li><a href="/Praktijk/PraktijkProjectThema3hp">Home</a></li>
+                    <li><a href="/Praktijk/Project/PraktijkProjectThema3/index.php">Home</a></li>
                     <li><a href="">Psychologen en locaties</a></li>
                     <li><a href="">Behandelingen</a></li>
                     <li><a href="">Vergoedingen</a></li>
