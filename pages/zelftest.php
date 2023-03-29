2b867ebf3238f2f71cf97a30c4e966dfa4e797be
jesse <jesse.copier@edu_kw1c.nl>
2023-03-29 11:26:49 +0200

    Zelftest pagina begin; aanpassing algemene stylesheet.

---
+++ pages/zelftest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * User: Jesse Copier
+ * Date: 22/03/2023 14:30
+ * File: zelftest.php
+ *
+ * Omschrijving: De pagina met de zelftest
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        zelftest.php
+    </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Koppeling CSS -->
+    <link href="../styles/stylesheet.css" rel="stylesheet">
+    <link href="../styles/zelftest.css" rel="stylesheet">
+</head>
+<body>
+    <?php
+    include("../includes/header.php");
+    ?>
+    <div id="zelftestBanner">
+        <article>
+            <h2>
+                Zelftest
+            </h2>
+        </article>
+    </div>
+</body>
+</html>
