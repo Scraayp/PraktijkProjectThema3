16063f5d9c13411301adc41b87d2a8f59ea105e1
jesse <jesse.copier@edu_kw1c.nl>
2023-03-29 12:38:00 +0200

    Zelftest deel 2

--- pages/zelftest.php
+++ pages/zelftest.php
@@ -26,10 +26,30 @@
     ?>
     <div id="zelftestBanner">
         <article>
-            <h2>
+            <h1>
                 Zelftest
+            </h1>
+            <h2>
+                Bepaal welke persoonlijkheid bij jou past!
             </h2>
+            <p class="headerP">
+                <strong>
+                    De DISC-methode categoriseert persoonlijkheden in vier types: Dominant, Invloedrijk, Stabiel en Consciëntieus.
+                    Bij elke vraag kies je het antwoord dat het beste bij jou past.
+                    Elke vraag heeft vier mogelijke antwoorden.
+                    Aan het einde van de test wordt je score berekend en krijg je te zien welke persoonlijkheid het best bij jou past!
+                </strong>
+            </p>
         </article>
     </div>
+    <main>
+        <form>
+            <div class="questionContainer">
+                <h3>
+                    Question
+                </h3>
+            </div>
+        </form>
+    </main>
 </body>
 </html>
